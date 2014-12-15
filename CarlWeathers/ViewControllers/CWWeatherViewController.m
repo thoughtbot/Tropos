45531bab4fd34dc8a08dd9b6bfeadc93450c5d3a
Keith Smiley <keithbsmiley@gmail.com>
2014-12-15 10:50:49 -0800

    Remove log

--- CarlWeathers/ViewControllers/CWWeatherViewController.m
+++ CarlWeathers/ViewControllers/CWWeatherViewController.m
@@ -124,7 +124,6 @@
             [self.view layoutIfNeeded];
         }];
     } errorBlock:^(NSError *error) {
-        NSLog(@"error: %@", error);
         if (error.code == CWErrorLocationDenied) {
             [self performSegueWithIdentifier:@"CWLocationDeniedViewController" sender:self];
         }
