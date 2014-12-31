a0939db12e0e4b5ec9aea35b8ee8bb77e2ecc374
Keith Smiley <keithbsmiley@gmail.com>
2014-12-30 20:05:09 -0800

    Setup HockeyApp

--- CarlWeathers/Controllers/CWAppDelegate.m
+++ CarlWeathers/Controllers/CWAppDelegate.m
@@ -1,5 +1,15 @@
+#import <HockeySDK/HockeySDK.h>
 #import "CWAppDelegate.h"
 
 @implementation CWAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    [[BITHockeyManager sharedHockeyManager] configureWithIdentifier:@"92f4164a0e066dfb541c8b9867fb8ebd"];
+    [[BITHockeyManager sharedHockeyManager] startManager];
+    [[[BITHockeyManager sharedHockeyManager] authenticator] authenticateInstallation];
+
+    return YES;
+}
 
 @end
