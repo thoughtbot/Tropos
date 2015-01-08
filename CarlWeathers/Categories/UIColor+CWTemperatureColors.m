6c66650dde9b2b81ad90c76eaf7306085b371f72
Keith Smiley <keithbsmiley@gmail.com>
2015-01-08 10:01:45 -0800

    Fix archive number type warnings

--- CarlWeathers/Categories/UIColor+CWTemperatureColors.m
+++ CarlWeathers/Categories/UIColor+CWTemperatureColors.m
@@ -9,22 +9,22 @@
 
 + (instancetype)hotColor
 {
-    return [UIColor colorWithHue:11.0 / 350.0 saturation:0.80 brightness:0.92 alpha:1.0];
+    return [UIColor colorWithHue:11.0f / 350.0f saturation:0.80f brightness:0.92f alpha:1.0f];
 }
 
 + (instancetype)warmerColor
 {
-    return [UIColor colorWithHue:40.0 / 350.0 saturation:1.0 brightness:0.97 alpha:1.0];
+    return [UIColor colorWithHue:40.0f / 350.0f saturation:1.0f brightness:0.97f alpha:1.0f];
 }
 
 + (instancetype)coolerColor
 {
-    return [UIColor colorWithHue:194.0 / 350.0 saturation:1 brightness:0.93 alpha:1.0];
+    return [UIColor colorWithHue:194.0f / 350.0f saturation:1.0f brightness:0.93f alpha:1.0f];
 }
 
 + (instancetype)coldColor
 {
-    return [UIColor colorWithHue:194.0 / 350.0 saturation:0.54 brightness:0.95 alpha:1.0];
+    return [UIColor colorWithHue:194.0f / 350.0f saturation:0.54f brightness:0.95f alpha:1.0f];
 }
 
 @end
