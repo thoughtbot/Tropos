8c6587aeff0d7a025e3aaf0b0333226d6b335413
Mark Adams <mark@thoughtbot.com>
2015-03-13 18:31:54 -0700

    Remove view model logic
    Now we just transform a weather update immediately and leverage that
    view model for all of our formatting. The controller simply serves as a
    reactive interface to the view model.

--- Tropos/Controllers/TRWeatherController.m
+++ Tropos/Controllers/TRWeatherController.m
@@ -1,18 +1,13 @@
 @import CoreLocation;
 #import "TRWeatherController.h"
+#import "TRWeatherUpdate.h"
 #import "TRLocationController.h"
 #import "TRForecastController.h"
 #import "TRSettingsController.h"
-#import "TRDateFormatter.h"
-#import "TRWeatherUpdate.h"
-#import "TRTemperatureFormatter.h"
-#import "TRWindSpeedFormatter.h"
-#import "TRTemperatureComparisonFormatter.h"
-#import "NSMutableAttributedString+TRAttributeHelpers.h"
 #import "TRGeocodeController.h"
-#import "TRDailyForecast.h"
 #import "TRDailyForecastViewModel.h"
 #import "TRAnalyticsController.h"
+#import "TRWeatherViewModel.h"
 
 @interface TRWeatherController ()
 
@@ -22,9 +17,8 @@
 @property (nonatomic) TRGeocodeController *geocodeController;
 @property (nonatomic) TRForecastController *forecastController;
 @property (nonatomic) TRSettingsController *settingsController;
-@property (nonatomic) TRDateFormatter *dateFormatter;
 
-@property (nonatomic) TRWeatherUpdate *weatherUpdate;
+@property (nonatomic) TRWeatherViewModel *viewModel;
 @property (nonatomic) NSError *weatherUpdateError;
 
 @end
@@ -42,16 +36,23 @@
     self.geocodeController = [TRGeocodeController new];
     self.forecastController = [TRForecastController new];
     self.settingsController = [TRSettingsController new];
-    self.dateFormatter = [TRDateFormatter new];
 
     @weakify(self)
     self.updateWeatherCommand = [[RACCommand alloc] initWithSignalBlock:^RACSignal *(id input) {
         @strongify(self)
-        return [self updateWeatherSignal];
+        return [[[[self.locationController requestWhenInUseAuthorization] then:^RACSignal *{
+            return [self.locationController updateCurrentLocation];
+        }] flattenMap:^RACStream *(CLLocation *location) {
+            return [self.geocodeController reverseGeocodeLocation:location];
+        }] flattenMap:^RACStream *(CLPlacemark *placemark) {
+            return [self.forecastController fetchWeatherUpdateForPlacemark:placemark];
+        }];
     }];
 
-    RAC(self, weatherUpdate) = [[self.updateWeatherCommand.executionSignals switchToLatest] doNext:^(TRWeatherUpdate *weatherUpdate) {
-        [[TRAnalyticsController sharedController] trackEvent:weatherUpdate];
+    RAC(self, viewModel) = [[[self.updateWeatherCommand.executionSignals switchToLatest] doNext:^(TRWeatherUpdate *update) {
+        [[TRAnalyticsController sharedController] trackEvent:update];
+    }] map:^id(TRWeatherUpdate *update) {
+        return [[TRWeatherViewModel alloc] initWithWeatherUpdate:update];
     }];
 
     RAC(self, weatherUpdateError) = [self.updateWeatherCommand.errors doNext:^(NSError *error) {
@@ -63,19 +64,10 @@
 
 #pragma mark - Properties
 
-- (RACSignal *)weatherDisplayChanged
-{
-    return [RACSignal combineLatest:@[RACObserve(self, weatherUpdate), self.settingsController.unitSystemChanged] reduce:^id(TRWeatherUpdate *weatherUpdate) {
-        return weatherUpdate;
-    }];
-}
-
 - (RACSignal *)status
 {
     RACSignal *updating = [[self.updateWeatherCommand.executing ignore:@NO] mapReplace:nil];
-    RACSignal *success = [[RACObserve(self, weatherUpdate) ignore:nil] map:^id(TRWeatherUpdate *update) {
-        return [self.dateFormatter stringFromDate:update.date];
-    }];
+    RACSignal *success = [RACObserve(self, viewModel.updatedDateString) ignore:nil];
     RACSignal *error = [[RACObserve(self, weatherUpdateError) ignore:nil] mapReplace:nil];
 
     return [RACSignal merge:@[updating, success, error]];
@@ -84,9 +76,7 @@
 - (RACSignal *)locationName
 {
     RACSignal *startedLocating = [[self.updateWeatherCommand.executing ignore:@NO] mapReplace:NSLocalizedString(@"Checking Weather...", nil)];
-    RACSignal *updatedLocation = [RACObserve(self, weatherUpdate) map:^id(TRWeatherUpdate *update) {
-        return [NSString stringWithFormat:@"%@, %@", update.city, update.state];
-    }];
+    RACSignal *updatedLocation = RACObserve(self, viewModel.locationName);
     RACSignal *error = [[RACObserve(self, weatherUpdateError) ignore:nil] map:^id(id value) {
         return NSLocalizedString(@"Update Failed", nil);
     }];
@@ -96,128 +86,31 @@
 
 - (RACSignal *)conditionsImage
 {
-    return [[RACObserve(self, weatherUpdate) map:^id(TRWeatherUpdate *weatherUpdate) {
-        if (!weatherUpdate) return nil;
-        return [UIImage imageNamed:weatherUpdate.conditionsDescription];
-    }] startWith:nil];
+    return RACObserve(self, viewModel.conditionsImage);
 }
 
 - (RACSignal *)conditionsDescription
 {
-    return [[RACObserve(self, weatherUpdate) map:^id(TRWeatherUpdate *weatherUpdate) {
-        if (!weatherUpdate) return nil;
-        TRTemperatureComparison comparison = [self.weatherUpdate.currentTemperature comparedTo:self.weatherUpdate.yesterdaysTemperature];
-        
-        NSString *adjective;
-        NSString *comparisonString = [TRTemperatureComparisonFormatter localizedStringFromComparison:comparison adjective:&adjective];
-        
-        NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:comparisonString];
-        [attributedString setFont:[UIFont defaultUltraLightFontOfSize:37]];
-        [attributedString setTextColor:[UIColor defaultTextColor]];
-        TRTemperature *difference = [self.weatherUpdate.currentTemperature temperatureDifferenceFromTemperature:self.weatherUpdate.yesterdaysTemperature];
-        [attributedString setTextColor:[self colorForTemperatureComparison:comparison difference:difference.fahrenheitValue] forSubstring:adjective];
-        
-        return attributedString;
-    }] startWith:nil];
+    return RACObserve(self, viewModel.conditionsDescription);
 }
 
 - (RACSignal *)windDescription
 {
-    return [[[RACSignal combineLatest:@[RACObserve(self, weatherUpdate), self.settingsController.unitSystemChanged]] map:^id(RACTuple *tuple) {
-        RACTupleUnpack(TRWeatherUpdate *weatherUpdate, __unused NSNumber *unitSystem) = tuple;
-
-        if (!weatherUpdate) return nil;
-        return [TRWindSpeedFormatter localizedStringForWindSpeed:weatherUpdate.windSpeed bearing:weatherUpdate.windBearing];
-    }] startWith:nil];
+    return [[RACObserve(self, viewModel.windDescription) combineLatestWith:self.settingsController.unitSystemChanged] map:^id(id value) {
+        return self.viewModel.windDescription;
+    }];
 }
 
 - (RACSignal *)highLowTemperatureDescription
 {
-    return [[[RACSignal combineLatest:@[RACObserve(self, weatherUpdate), self.settingsController.unitSystemChanged]] map:^id(RACTuple *tuple) {
-        RACTupleUnpack(TRWeatherUpdate *weatherUpdate, __unused NSNumber *unitSystem) = tuple;
-
-        if (!weatherUpdate) return nil;
-        TRTemperatureFormatter *formatter = [TRTemperatureFormatter new];
-        NSString *high = [formatter stringFromTemperature:weatherUpdate.currentHigh];
-        NSString *current = [formatter stringFromTemperature:weatherUpdate.currentTemperature];
-        NSString *low = [formatter stringFromTemperature:weatherUpdate.currentLow];
-        NSString *temperatureString = [NSString stringWithFormat:@"%@ / %@ / %@", high, current, low];
-
-
-
-        NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:temperatureString];
-        TRTemperatureComparison comparison = [self.weatherUpdate.currentTemperature comparedTo:self.weatherUpdate.yesterdaysTemperature];
-        TRTemperature *difference = [self.weatherUpdate.currentTemperature temperatureDifferenceFromTemperature:self.weatherUpdate.yesterdaysTemperature];
-
-        NSRange rangeOfFirstSlash = [temperatureString rangeOfString:@"/"];
-        NSRange rangeOfLastSlash = [temperatureString rangeOfString:@"/" options:NSBackwardsSearch];
-        NSRange range = NSMakeRange(rangeOfFirstSlash.location + 1, rangeOfLastSlash.location - (rangeOfFirstSlash.location + 1));
-
-        [attributedString setTextColor:[self colorForTemperatureComparison:comparison difference:difference.fahrenheitValue] forRange:range];
-
-        return attributedString;
-    }] startWith:nil];
+    return [[RACObserve(self, viewModel.temperatureDescription) combineLatestWith:self.settingsController.unitSystemChanged] map:^id(RACTuple *tuple) {
+        return self.viewModel.temperatureDescription;
+    }];
 }
 
 - (RACSignal *)dailyForecastViewModels
 {
-    return [[[RACObserve(self, weatherUpdate) ignore:nil] map:^id(TRWeatherUpdate *weatherUpdate) {
-        NSMutableArray *viewModels = [NSMutableArray array];
-        for (TRDailyForecast *forecast in weatherUpdate.dailyForecasts) {
-            TRDailyForecastViewModel *viewModel = [[TRDailyForecastViewModel alloc] initWithDailyForecast:forecast];
-            [viewModels addObject:viewModel];
-        }
-        return [viewModels copy];
-    }] startWith:nil];
-}
-
-#pragma mark - Public Methods
-
-- (RACSignal *)updateWeatherSignal
-{
-    return [[[[self.locationController requestWhenInUseAuthorization] then:^RACSignal *{
-        return [self.locationController updateCurrentLocation];
-    }] flattenMap:^RACStream *(CLLocation *location) {
-        return [self.geocodeController reverseGeocodeLocation:location];
-    }] flattenMap:^RACStream *(CLPlacemark *placemark) {
-        return [self.forecastController fetchWeatherUpdateForPlacemark:placemark];
-    }];
-}
-
-#pragma mark - Private Methods
-
-- (UIColor *)colorForTemperatureComparison:(TRTemperatureComparison)comparison difference:(NSInteger)difference
-{
-    UIColor *color;
-
-    switch (comparison) {
-        case TRTemperatureComparisonSame:
-            color = [UIColor defaultTextColor];
-            break;
-        case TRTemperatureComparisonColder:
-            color = [UIColor coldColor];
-            break;
-        case TRTemperatureComparisonCooler:
-            color = [UIColor coolerColor];
-            break;
-        case TRTemperatureComparisonHotter:
-            color = [UIColor hotColor];
-            break;
-        case TRTemperatureComparisonWarmer:
-            color = [UIColor warmerColor];
-            break;
-    }
-
-    if (comparison == TRTemperatureComparisonCooler || comparison == TRTemperatureComparisonWarmer) {
-        CGFloat amount = MIN(ABS(difference), 10) / 10.0f;
-        CGFloat lighterAmount = 1 - amount;
-
-        if (lighterAmount > 0.80) lighterAmount = 0.80f;
-
-        color = [color lighterColorByAmount:lighterAmount];
-    }
-
-    return color;
+    return RACObserve(self, viewModel.dailyForecasts);
 }
 
 @end
