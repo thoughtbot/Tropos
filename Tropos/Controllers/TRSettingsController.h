0fb14d6e4816ef87c4ccb50a04cab5d07714a2a6
Mark Adams <mark.adams86@gmail.com>
2015-04-27 18:58:21 -0700

    Make unitSystem a property
    This was going to be annoying when accessed with swift.

--- Tropos/Controllers/TRSettingsController.h
+++ Tropos/Controllers/TRSettingsController.h
@@ -8,10 +8,8 @@ typedef NS_ENUM(NSInteger, TRUnitSystem) {
 @interface TRSettingsController : NSObject
 
 @property (nonatomic, readonly) RACSignal *unitSystemChanged;
+@property (nonatomic) TRUnitSystem unitSystem;
 
 - (void)registerSettings;
-
-- (TRUnitSystem)unitSystem;
-- (void)setUnitSystem:(TRUnitSystem)unitSystem;
 
 @end
