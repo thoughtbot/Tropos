fca51989af562af57a6eebe70947c1beff8b4b17
Mark Adams <mark.adams86@gmail.com>
2015-04-30 13:20:48 -0700

    Set spacing back to 4 spaces

--- Tropos/Controllers/AppearanceController.swift
+++ Tropos/Controllers/AppearanceController.swift
@@ -1,10 +1,10 @@
-  static func configureAppearance() {
-    let navigationBar = UINavigationBar.appearance()
-    navigationBar.titleTextAttributes = [NSFontAttributeName: UIFont.defaultLightFontOfSize(20), NSForegroundColorAttributeName: UIColor.lightTextColor()]
-    navigationBar.tintColor = .lightTextColor()
 @objc final class AppearanceController {
+    static func configureAppearance() {
+        let navigationBar = UINavigationBar.appearance()
+        navigationBar.titleTextAttributes = [NSFontAttributeName: UIFont.defaultLightFontOfSize(20), NSForegroundColorAttributeName: UIColor.lightTextColor()]
+        navigationBar.tintColor = .lightTextColor()
 
-    let attributes = [NSFontAttributeName: UIFont.defaultLightFontOfSize(17), NSForegroundColorAttributeName: UIColor.lightTextColor()]
-    UIBarButtonItem.appearance().setTitleTextAttributes(attributes, forState: .Normal)
-  }
+        let attributes = [NSFontAttributeName: UIFont.defaultLightFontOfSize(17), NSForegroundColorAttributeName: UIColor.lightTextColor()]
+        UIBarButtonItem.appearance().setTitleTextAttributes(attributes, forState: .Normal)
+    }
 }
--- Tropos/Views/TRTableViewCell.swift
+++ Tropos/Views/TRTableViewCell.swift
@@ -1,6 +1,6 @@
 class TRTableViewCell: UITableViewCell {
-  override func awakeFromNib() {
-    super.awakeFromNib()
-    textLabel?.font = UIFont.defaultLightFontOfSize(18.0)
-  }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        textLabel?.font = UIFont.defaultLightFontOfSize(18.0)
+    }
 }
