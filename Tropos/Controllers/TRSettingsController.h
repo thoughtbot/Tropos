f908d02bf9926a9a14e8c3daff6f6805860c4e33
Adam Sharp <adsharp@me.com>
2016-04-18 11:34:16 -0400

    fixup! Replace `-unitSystemChanged` with simpler category that uses KVO

--- Tropos/Controllers/TRSettingsController.h
+++ Tropos/Controllers/TRSettingsController.h
@@ -1,5 +1,4 @@
 #import <Foundation/Foundation.h>
-#import <ReactiveCocoa/ReactiveCocoa.h>
 
 FOUNDATION_EXPORT NSString *const TRSettingsLastVersionKey;
 FOUNDATION_EXPORT NSString *const TRSettingsUnitSystemKey;
