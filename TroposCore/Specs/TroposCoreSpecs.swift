10d78b306af8be98853ba1135d37e05acc396467
Adam Sharp <adsharp@me.com>
2016-04-20 10:33:03 -0400

    Add TroposCore-iOS framework target

---
+++ TroposCore/Specs/TroposCoreSpecs.swift
@@ -0,0 +1,11 @@
+@testable import TroposCore
+import Quick
+import Nimble
+
+final class TroposCoreSpecs: QuickSpec {
+    override func spec() {
+        it("works") {
+            expect(1 + 1) == 2
+        }
+    }
+}
