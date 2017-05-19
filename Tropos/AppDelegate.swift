1cbf6878d08ce2cce3b891c9aa0bfcc5de451d69
James Martinez <jamescmartinez@gmail.com>
2017-05-19 13:29:01 -0700

    Code views instead of storyboards

--- Tropos/AppDelegate.swift
+++ Tropos/AppDelegate.swift
@@ -2,10 +2,14 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-  var window: UIWindow?
+  var window: UIWindow? = {
+    let window = UIWindow(frame: UIScreen.main.bounds)
+    window.rootViewController = ViewController()
+    return window
+  }()
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    window?.makeKeyAndVisible()
     return true
   }
 }
