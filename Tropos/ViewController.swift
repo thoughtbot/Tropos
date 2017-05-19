b1a8968e45260f0df85e481ff60c404cf52a55fa
James Martinez <jamescmartinez@gmail.com>
2017-05-19 13:17:15 -0700

    Initial commit for rewrite

---
+++ Tropos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tropos
+//
+//  Created by James Martinez on 5/19/17.
+//  Copyright © 2017 thoughtbot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
