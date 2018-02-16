5c0e96b1264d67a8619f08a2a4ca81a70d065ede
Adam Sharp & Daniel Barber <adam+db@thoughtbot.com>
2018-02-16 16:34:12 -0500

    Fix localized string bug introduced with lowercase enum cases
    Lowercasing our enum cases had the unintended side effect of breaking
    localized string lookup that relied upon the case name. Explicitly
    capitalising the case name resolves the issue.

--- Core/Sources/Formatters/PrecipitationChanceFormatter.swift
+++ Core/Sources/Formatters/PrecipitationChanceFormatter.swift
@@ -2,7 +2,7 @@ import Foundation
 
 struct PrecipitationChanceFormatter {
     func localizedStringFromPrecipitation(_ precipitation: Precipitation) -> String {
-        let adjective = precipitation.chance.description
+        let adjective = precipitation.chance.description.capitalized
         let type = precipitation.type.capitalized
         return TroposCoreLocalizedString(adjective + type)
     }
