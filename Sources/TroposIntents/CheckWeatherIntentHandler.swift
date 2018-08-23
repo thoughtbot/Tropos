81b5a06e7f58085d7496c2288508a113a028f4db
Adam Sharp <adam@sharplet.me>
2018-08-23 14:51:37 -0400

    Dynamically update weather from check weather intent
    Instead of reading the latest cached version, we now always fetch the
    latest weather update and cache it before reading the description to the
    user.

--- Sources/TroposIntents/CheckWeatherIntentHandler.swift
+++ Sources/TroposIntents/CheckWeatherIntentHandler.swift
@@ -1,18 +1,56 @@
 import Foundation
+import ReactiveSwift
+import Result
 import TroposCore
+import os.log
 
 @available(iOS 12.0, *)
 public final class CheckWeatherIntentHandler: NSObject, CheckWeatherIntentHandling {
     public func handle(intent: CheckWeatherIntent, completion: @escaping (CheckWeatherIntentResponse) -> Void) {
-        let cache = WeatherUpdateCache(fileName: WeatherUpdateCache.latestWeatherUpdateFileName)
-        let response: CheckWeatherIntentResponse
-        defer { completion(response) }
+        DispatchQueue.main.async {
+            self.updateWeather(completion: completion)
+        }
+    }
 
-        if let weatherUpdate = cache?.latestWeatherUpdate {
-            let viewModel = WeatherViewModel(weatherUpdate: weatherUpdate)
-            response = CheckWeatherIntentResponse.success(conditionsDescription: viewModel.conditionsDescription.string)
-        } else {
-            response = CheckWeatherIntentResponse(code: .failure, userActivity: nil)
+    func updateWeather(completion: @escaping (CheckWeatherIntentResponse) -> Void) {
+        let forecast = ForecastController(apiKey: TRForecastAPIKey)
+        let geocode = GeocodeController()
+        let location = LocationController()
+
+        let weatherUpdate = location.requestAlwaysAuthorization()
+            .flatMap(.latest) { _ in location.updateCurrentLocation() }
+            .flatMap(.latest) { location in geocode.reverseGeocode(location) }
+            .mapError(AnyError.init)
+            .flatMap(.latest) { placemark in forecast.fetchWeatherUpdate(for: placemark) }
+
+        weatherUpdate.startWithResult { result in
+            switch result {
+            case let .success(weatherUpdate):
+                self.cacheWeatherUpdate(weatherUpdate) {
+                    let viewModel = WeatherViewModel(weatherUpdate: weatherUpdate)
+                    completion(.success(conditionsDescription: viewModel.conditionsDescription.string))
+                }
+            case .failure:
+                completion(.init(code: .failure, userActivity: nil))
+            }
+        }
+    }
+
+    func cacheWeatherUpdate(_ weatherUpdate: WeatherUpdate, completion: @escaping () -> Void) {
+        guard let cache = WeatherUpdateCache(fileName: WeatherUpdateCache.latestWeatherUpdateFileName) else {
+            completion()
+            return
+        }
+
+        cache.archiveWeatherUpdate(weatherUpdate) { isSuccess, error in
+            defer { completion() }
+            if isSuccess { return }
+
+            if let error = error {
+                os_log("Failed to archive weather update: %{public}@", type: .error, error.localizedDescription)
+            } else {
+                os_log("Failed to archive weather update", type: .error)
+            }
         }
     }
 }
---
+++ Sources/TroposIntents/TroposIntents-Bridging-Header.h
@@ -0,0 +1,1 @@
+#import "Secrets.h"
