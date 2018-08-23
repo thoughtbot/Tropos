0e309d2f0526b1492c110989d8eebc8f1cb61d9e
Adam Sharp <adam@sharplet.me>
2018-08-23 14:55:08 -0400

    Don't crash when fonts aren't available to Siri intent

--- Sources/Tropos/Controllers/AppearanceController.swift
+++ Sources/Tropos/Controllers/AppearanceController.swift
@@ -5,13 +5,13 @@ import TroposCore
     @objc static func configureAppearance() {
         let navigationBar = UINavigationBar.appearance()
         navigationBar.titleTextAttributes = [
-            .font: UIFont.defaultLightFont(size: 20),
+            .font: UIFont.defaultLightFont(size: 20)!,
             .foregroundColor: UIColor.lighterTextColor,
         ]
         navigationBar.tintColor = .lighterTextColor
 
         let attributes: [NSAttributedStringKey: Any] = [
-            .font: UIFont.defaultLightFont(size: 17),
+            .font: UIFont.defaultLightFont(size: 17)!,
             .foregroundColor: UIColor.lighterTextColor,
         ]
         UIBarButtonItem.appearance().setTitleTextAttributes(attributes, for: .normal)
--- Sources/TroposCore/Extensions/UIFont.swift
+++ Sources/TroposCore/Extensions/UIFont.swift
@@ -1,11 +1,11 @@
 import UIKit
 
 extension UIFont {
-    @objc public static func defaultLightFont(size: CGFloat) -> UIFont {
-        return UIFont(name: "DINNextLTPro-Light", size: size)!
+    @objc public static func defaultLightFont(size: CGFloat) -> UIFont? {
+        return UIFont(name: "DINNextLTPro-Light", size: size)
     }
 
-    @objc public static func defaultRegularFont(size: CGFloat) -> UIFont {
-        return UIFont(name: "DINNextLTPro-Regular", size: size)!
+    @objc public static func defaultRegularFont(size: CGFloat) -> UIFont? {
+        return UIFont(name: "DINNextLTPro-Regular", size: size)
     }
 }
